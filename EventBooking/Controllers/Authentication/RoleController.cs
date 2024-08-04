2b17047c2b89b85487446798ad9a4452f01294c3
giaphu290 <phutg2000@gmail.com>
2024-08-05 00:34:36 +0700

    Restore EventBooking as a regular directory

---
+++ EventBooking/Controllers/Authentication/RoleController.cs
@@ -0,0 +1,35 @@
+﻿using EventBooking.Application.Common.Constants;
+using EventBooking.Application.Common.Services.Interfaces;
+using EventBooking.Application.Features.Auth.RoleManage.Models;
+using EventBooking.Application.Features.Auth.RoleManage.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventBooking.API.Controllers.Authentication
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly IMediatorService _mediatorService;
+
+        public RoleController(IMediatorService mediatorService)
+        {
+            _mediatorService = mediatorService;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("get-all")]
+        public async Task<ActionResult<RoleResponse>> GetAllRole()
+        {
+            var user = await _mediatorService.Send(new GetRoleQuery());
+            return Ok(new BaseResponseModel<IEnumerable<GetRoleResponse>>(
+                statusCode: StatusCodes.Status200OK,
+                code: ResponseCodeConstants.SUCCESS,
+                data: user));
+        }
+    }
+}
